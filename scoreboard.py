7636ae51d2f0dd6da789a1829239e9c05fa188f9
Eren ERDOĞAN <imerenerdogan@gmail.com>
2020-12-02 11:56:13 +0300

    Snake Game v0.2
    Added a simple high score system that keeps the record of high score even after close and reopen the game

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as hs:
+            self.high_score = int(hs.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,13 +17,17 @@ class Scoreboard(Turtle):
         self.hideturtle()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER!", align=ALIGNMENT, font=("Courier", 24, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            with open("data.txt", mode="w") as hs:
+                hs.write(str(self.score))
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
-        self.clear()
         self.score += 1
         self.update_scoreboard()
